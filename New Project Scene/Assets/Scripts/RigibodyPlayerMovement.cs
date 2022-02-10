57c44c0f5b7a09121a7f6a4d66d9ed1c8e839317
PedroSilva1822 <pedrohsilva0928@gmail.com>
2022-02-09 21:50:37 -0500

    New Project Scene

---
+++ New Project Scene/Assets/Scripts/RigibodyPlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigibodyPlayerMovement : MonoBehaviour
+{
+    /* public float moveSpeed;
+    private Rigibody rigibody;*/
+
+    public float moveSpeed;// Controls player movement speed.
+    private Rigidbody rigibody;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rigibody = GetComponent<Rigidbody>();//The moment the script is active, retrieve the rigibody component of the Gameobject.
+    }
+    void Start()
+    {
+        
+    }
+
+    //Update is called once per frame
+    private void FixedUpdate()//Prefered update when using anything resolving around physics. Ensures consistent functionality regardless of frame
+    {
+        rigibody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rigibody.velocity.y, Input.GetAxis("Vertical")) * moveSpeed; Debug.Log("Currently detecting" + rigibody);
+    }
+}
